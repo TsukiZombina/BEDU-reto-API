83e7b9ffb5346be70328c7137a1e3b6f88d58cb1
Nathaniel Ceballos <ceballos.equihua@gmail.com>
2022-08-25 22:24:11 -0500

    Get poke data from search field

--- src/libs/bootstrap/js/apiPokemonBusqueda/busqueda.js
+++ src/libs/bootstrap/js/apiPokemonBusqueda/busqueda.js
@@ -1,6 +1,32 @@
+const app = document.getElementById('app');
+
+let pokeData;
+
+function getPokemonData() {
+    return fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=2000')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            pokeData = data.results;
+            console.log("Test");
+            console.log(pokeData);
+        })
+}
+
+if (pokeData == null) {
+    getPokemonData()
+}
+
+console.log(pokeData)
+
 function test() {
-    input_text = document.getElementById("input-search1").value
-    console.log(input_text)
+    let input_text = document.getElementById("input-search1").value
+    let test_search = pokeData.filter(obj => {
+        return obj.name === input_text.toLowerCase()
+    })
+    console.log(test_search)
 }
 
 document.getElementById("search1").addEventListener("click", test)
