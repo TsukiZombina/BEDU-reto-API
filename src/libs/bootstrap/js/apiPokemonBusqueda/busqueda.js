f0c2536fde14c35068f40fdb6b5e47ade759adbe
Nathaniel Ceballos <ceballos.equihua@gmail.com>
2022-08-25 23:02:38 -0500

    Retrieves basic info for all pokémon
    Also does a basic search by name

--- src/libs/bootstrap/js/apiPokemonBusqueda/busqueda.js
+++ src/libs/bootstrap/js/apiPokemonBusqueda/busqueda.js
@@ -1,32 +1,52 @@
 const app = document.getElementById('app');
 
-let pokeData;
+let pokeDataList;
 
-function getPokemonData() {
-    return fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=2000')
+// Gets name, id, normal version image url and shiny version image url for each pokémon
+getPokemonList()
+    .then(function (data) {
+
+        console.log(data);
+
+        pokeDataList = [];
+        
+        data.results.forEach(function (dataPoke) {
+            fetch(dataPoke.url)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (pokeData) {
+                    const name = pokeData.name
+                    const id = pokeData.id
+                    const imgNormal = pokeData.sprites.front_default
+                    const imgShiny = pokeData.sprites.front_shiny
+                    let pokemon = { name: name, id: id, imgUrlNormal: imgNormal, imgUrlShiny: imgShiny }
+                    pokeDataList.push(pokemon)
+                } )
+          })
+
+        console.log("PokeDataList");
+        console.log(pokeDataList);
+    })
+
+
+function getPokemonList() {
+    return fetch('https://pokeapi.co/api/v2/pokemon?limit=2000')
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            console.log(data);
-            pokeData = data.results;
-            console.log("Test");
-            console.log(pokeData);
-        })
 }
 
-if (pokeData == null) {
-    getPokemonData()
+if (pokeDataList == null) {
+    getPokemonList()
 }
 
-console.log(pokeData)
-
-function test() {
+function search1() {
     let input_text = document.getElementById("input-search1").value
-    let test_search = pokeData.filter(obj => {
+    let test_search = pokeDataList.filter(obj => {
         return obj.name === input_text.toLowerCase()
     })
     console.log(test_search)
 }
 
-document.getElementById("search1").addEventListener("click", test)
+document.getElementById("search1").addEventListener("click", search1)
