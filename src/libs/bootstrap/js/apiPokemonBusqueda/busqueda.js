8a801f7b74a41a727c351acff3f540db6167a0fb
Nathaniel Ceballos <ceballos.equihua@gmail.com>
2022-08-25 22:01:21 -0500

    Test function retrieve text field value

--- src/libs/bootstrap/js/apiPokemonBusqueda/busqueda.js
+++ src/libs/bootstrap/js/apiPokemonBusqueda/busqueda.js
@@ -1,5 +1,6 @@
 function test() {
-    console.log("test")
+    input_text = document.getElementById("input-search1").value
+    console.log(input_text)
 }
 
 document.getElementById("search1").addEventListener("click", test)
